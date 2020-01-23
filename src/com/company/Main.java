cce6973ced2453fca6e85eac149f3f0e7178914f
nameMark <marko.dufala@gmail.com>
2020-01-23 20:57:26 +0100

    updated database

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,39 +3,76 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.*;
 import java.text.*;
+import java.util.Scanner;
+import java.util.Date;
+
+
 public class Main {
 
     public static void main(String[] args) {
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("E yyyy/MM/dd HH-mm-ss ");
+        Scanner scanner = new Scanner(System.in);
+        int yourHoursInYourTimeZone;
+        int yourMinutesInYourTimeZone;
+        int yourSecondsInYourTimeZone;
 
 
-        long yourTimeZoneHour = date.getHours();
-        long yourTimeZoneMinutes = date.getMinutes();
-        long yourTimeZoneSeconds = date.getSeconds();
-
-        System.out.println(yourTimeZoneHour + " hours " +
-                yourTimeZoneMinutes + " minutes " + yourTimeZoneSeconds + " seconds" + " passed this day :D");
-
-        int timeZone = date.getTimezoneOffset();
-        System.out.println(timeZone);
-
-        ZonedDateTime.now(           // Capture the current moment in the wall-clock time used by the people of a certain region (a time zone).
-                ZoneId.systemDefault()   // Get the JVM’s current default time zone. Can change at any moment during runtime. If important, confirm with the user.
-        );                            // Renders
+        String houwManyHoursYouWantToAdd;
 
 
         Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
-        numbers.put("one", 1);
-        numbers.put("two", 2);
+        numbers.put("one",   1);
+        numbers.put("two",   2);
         numbers.put("three", 3);
+        numbers.put("four",  4);
+        numbers.put("five",  5);
+        numbers.put("six",   6);
+        numbers.put("seven", 7);
+        numbers.put("eight", 8);
+        numbers.put("nine",  9);
+        numbers.put("ten",  10);
 
-        System.out.println(numbers.get("one"));
 
 
-        Integer n = numbers.get("two");
-        if (n != null) {
-            System.out.println("two = " + n);
+
+
+
+
+
+
+        while (true) {
+
+
+
+            System.out.print("How many hours you want to add to your time? :");
+            houwManyHoursYouWantToAdd = scanner.nextLine();
+            // First String input
+
+            if ((houwManyHoursYouWantToAdd.equals("one")) || (houwManyHoursYouWantToAdd.equals("two")) || (houwManyHoursYouWantToAdd.equals("three")) || (houwManyHoursYouWantToAdd.equals("four")) ||
+                (houwManyHoursYouWantToAdd.equals("five")) || (houwManyHoursYouWantToAdd.equals("sixe")) || (houwManyHoursYouWantToAdd.equals("seven")) || (houwManyHoursYouWantToAdd.equals("eight")) ||
+                    (houwManyHoursYouWantToAdd.equals("nine")) || (houwManyHoursYouWantToAdd.equals("ten"))){
+                Integer n = numbers.get(houwManyHoursYouWantToAdd);
+                yourHoursInYourTimeZone = date.getHours();
+                yourMinutesInYourTimeZone = date.getMinutes();
+                yourSecondsInYourTimeZone = date.getSeconds();
+
+
+
+
+                if (n != null) {
+                    System.out.println(yourHoursInYourTimeZone + n + " hour(s)" + " " + yourMinutesInYourTimeZone + " minute(s)" + " " + yourSecondsInYourTimeZone + (" second(s)"));
+                }
+
+                break;
+            }
+            System.out.println("Invalid input! Enter value again.:");
         }
+
+
+
+
+
+
     }
 }
