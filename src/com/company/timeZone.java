86a6da617ecdf761c6be25969f6d1e353b3e9309
Marko <50919055+nameMark@users.noreply.github.com>
2020-01-23 20:10:22 +0100

    Update timeZone.java

--- src/com/company/timeZone.java
+++ src/com/company/timeZone.java
@@ -21,3 +21,53 @@ public class timeZone {
     }
 
 }
+
+
+
+
+
+
+
+
+package com.company;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.text.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("E yyyy/MM/dd HH-mm-ss ");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        long yourTimeZoneHour = date.getHours();
+        long yourTimeZoneMinutes = date.getMinutes();
+        long yourTimeZoneSeconds = date.getSeconds();
+
+        System.out.println("your time plus one hour " + yourTimeZoneHour+1);
+        System.out.println(yourTimeZoneHour + " hours " +
+                yourTimeZoneMinutes + " minutes " + yourTimeZoneSeconds + " seconds" + " passed this day :D");
+
+
+
+        String en = scanner.nextLine();
+
+        Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
+        numbers.put("one", 1);
+        numbers.put("two", 2);
+        numbers.put("three", 3);
+
+        System.out.println(numbers.get(en));
+
+
+        Integer n = numbers.get(en);
+        if (n != null)
+            System.out.println("two = " + n);
+        
+    }
+}
