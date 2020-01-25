80f62a8ba48bc93094b579c3056925d7d83c7c2a
nameMark <marko.dufala@gmail.com>
2020-01-25 15:56:16 +0100

    done database

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,17 +22,31 @@ public class Main {
 
 
         Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
-        numbers.put("one", 1);
-        numbers.put("two", 2);
-        numbers.put("three", 3);
-        numbers.put("four", 4);
-        numbers.put("five", 5);
-        numbers.put("six", 6);
-        numbers.put("seven", 7);
-        numbers.put("eight", 8);
-        numbers.put("nine", 9);
-        numbers.put("ten", 10);
-        numbers.put("eleven", 11);
+        numbers.put("Alofi", -12);
+        numbers.put("Honolulu", -11);
+        numbers.put("Juneu", -10);
+        numbers.put("Los Angeles", -9);
+        numbers.put("San Luis", -8);
+        numbers.put("Oklahoma City", -7);
+        numbers.put("Washington", -6);
+        numbers.put("San Juan", -5);
+        numbers.put("Rio de Janeiro", -4);
+        numbers.put("King Edward Point", -3);
+        numbers.put("Ponta Delgada", -2);
+        numbers.put("Lisabon", -1);
+        numbers.put("Bratislava", 0);
+        numbers.put("Sofia", 1);
+        numbers.put("Ankara", 2);
+        numbers.put("Dubai", 3);
+        numbers.put("Amsterdam Island", 4);
+        numbers.put("Dhaka", 5);
+        numbers.put("Bangkok", 6);
+        numbers.put("Shanghai", 7);
+        numbers.put("Tokyo", 8);
+        numbers.put("Sydney", 9);
+        numbers.put("Kingston", 10);
+        numbers.put("Jabat", 11);
+        numbers.put("Nukunonu", 12);
 
 
         while (true) {
@@ -42,9 +56,15 @@ public class Main {
             houwManyHoursYouWantToAdd = scanner.nextLine();
             // First String input
 
-            if ((houwManyHoursYouWantToAdd.equals("one")) || (houwManyHoursYouWantToAdd.equals("two")) || (houwManyHoursYouWantToAdd.equals("three")) || (houwManyHoursYouWantToAdd.equals("four")) ||
-                    (houwManyHoursYouWantToAdd.equals("five")) || (houwManyHoursYouWantToAdd.equals("sixe")) || (houwManyHoursYouWantToAdd.equals("seven")) || (houwManyHoursYouWantToAdd.equals("eight")) ||
-                    (houwManyHoursYouWantToAdd.equals("nine")) || (houwManyHoursYouWantToAdd.equals("ten")) || (houwManyHoursYouWantToAdd.equals("eleven"))) {
+
+            if ((   (houwManyHoursYouWantToAdd.equals("Alofi"))
+                    || (houwManyHoursYouWantToAdd.equals("Honolulu")) || (houwManyHoursYouWantToAdd.equals("Juneu")) || (houwManyHoursYouWantToAdd.equals("Los Angeles"))
+                    || (houwManyHoursYouWantToAdd.equals("San Luis")) ||(houwManyHoursYouWantToAdd.equals("Oklahoma City"))|| (houwManyHoursYouWantToAdd.equals("WashingTon"))|| (houwManyHoursYouWantToAdd.equals("San Juan"))
+                    || (houwManyHoursYouWantToAdd.equals("Rio de Janeiro")) || (houwManyHoursYouWantToAdd.equals("King Edward Point")) || (houwManyHoursYouWantToAdd.equals("Ponta Delgada"))
+                    || (houwManyHoursYouWantToAdd.equals("Lisabon"))|| (houwManyHoursYouWantToAdd.equals("Bratislava"))|| (houwManyHoursYouWantToAdd.equals("Sofia"))|| (houwManyHoursYouWantToAdd.equals("Ankara"))
+                    || (houwManyHoursYouWantToAdd.equals("Dubai"))|| (houwManyHoursYouWantToAdd.equals("Amsterdam Island")) || (houwManyHoursYouWantToAdd.equals("Dhaka")) || (houwManyHoursYouWantToAdd.equals("Bangkok"))
+                    || (houwManyHoursYouWantToAdd.equals("Shanghai"))|| (houwManyHoursYouWantToAdd.equals("Tokyo")) || (houwManyHoursYouWantToAdd.equals("Sydney")) || (houwManyHoursYouWantToAdd.equals("Kingston"))
+                    || (houwManyHoursYouWantToAdd.equals("Jabat")) || (houwManyHoursYouWantToAdd.equals("Nukunonu")))) {
                 Integer n = numbers.get(houwManyHoursYouWantToAdd);
                 yourHoursInYourTimeZone = date.getHours();
                 yourMinutesInYourTimeZone = date.getMinutes();
