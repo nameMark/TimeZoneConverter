ff1fa6b0004abf8c231c3f6ffdfe650a376639ee
nameMark <marko.dufala@gmail.com>
2020-01-25 15:04:14 +0100

    error catching for time more than 24 hours finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,27 +22,20 @@ public class Main {
 
 
         Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
-        numbers.put("one",   1);
-        numbers.put("two",   2);
+        numbers.put("one", 1);
+        numbers.put("two", 2);
         numbers.put("three", 3);
-        numbers.put("four",  4);
-        numbers.put("five",  5);
-        numbers.put("six",   6);
+        numbers.put("four", 4);
+        numbers.put("five", 5);
+        numbers.put("six", 6);
         numbers.put("seven", 7);
         numbers.put("eight", 8);
-        numbers.put("nine",  9);
-        numbers.put("ten",  10);
-
-
-
-
-
-
-
+        numbers.put("nine", 9);
+        numbers.put("ten", 10);
+        numbers.put("eleven", 11);
 
 
         while (true) {
-
 
 
             System.out.print("How many hours you want to add to your time? :");
@@ -50,32 +43,24 @@ public class Main {
             // First String input
 
             if ((houwManyHoursYouWantToAdd.equals("one")) || (houwManyHoursYouWantToAdd.equals("two")) || (houwManyHoursYouWantToAdd.equals("three")) || (houwManyHoursYouWantToAdd.equals("four")) ||
-                (houwManyHoursYouWantToAdd.equals("five")) || (houwManyHoursYouWantToAdd.equals("sixe")) || (houwManyHoursYouWantToAdd.equals("seven")) || (houwManyHoursYouWantToAdd.equals("eight")) ||
-                    (houwManyHoursYouWantToAdd.equals("nine")) || (houwManyHoursYouWantToAdd.equals("ten"))){
+                    (houwManyHoursYouWantToAdd.equals("five")) || (houwManyHoursYouWantToAdd.equals("sixe")) || (houwManyHoursYouWantToAdd.equals("seven")) || (houwManyHoursYouWantToAdd.equals("eight")) ||
+                    (houwManyHoursYouWantToAdd.equals("nine")) || (houwManyHoursYouWantToAdd.equals("ten")) || (houwManyHoursYouWantToAdd.equals("eleven"))) {
                 Integer n = numbers.get(houwManyHoursYouWantToAdd);
                 yourHoursInYourTimeZone = date.getHours();
                 yourMinutesInYourTimeZone = date.getMinutes();
                 yourSecondsInYourTimeZone = date.getSeconds();
 
 
-
-
                 if (n != null) {
                     if (yourHoursInYourTimeZone + n > 24) {
-                        yourHoursInYourTimeZoneNew = (yourHoursInYourTimeZone - 24);
+                        System.out.println( yourHoursInYourTimeZone + n + (-24) + " hour(s)" + " " + yourMinutesInYourTimeZone + " minute(s)" + " " + yourSecondsInYourTimeZone + (" second(s)"));
+                    }else if (yourHoursInYourTimeZone <= 24){
+                        System.out.println(yourHoursInYourTimeZone + n + " hour(s)" + " " + yourMinutesInYourTimeZone + " minute(s)" + " " + yourSecondsInYourTimeZone + (" second(s)"));
                     }
-                    System.out.println(yourHoursInYourTimeZoneNew + n + " hour(s)" + " " + yourHoursInYourTimeZoneNew + " minute(s)" + " " + yourHoursInYourTimeZoneNew + (" second(s)"));
                 }
-
                 break;
             }
             System.out.println("Invalid input! Enter value again.:");
         }
-
-
-
-
-
-
     }
 }
